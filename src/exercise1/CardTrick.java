84a6b5bb63c21c21b7e9b9aa1dc80cdb2301914d
J-asmeenkaur <156931852+J-asmeenkaur@users.noreply.github.com>
2024-01-31 16:36:45 -0500

    Update CardTrick.java
    entered info

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -43,17 +43,17 @@ public class CardTrick {
     private static void printInfo() {
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
-        System.out.println("My name is [Your Name].");
+        System.out.println("My name is Jasmeen Kaur.");
         System.out.println();
-        System.out.println("My career ambitions:");
-        System.out.println("-- Be more active on LinkedIn");
-        System.out.println("-- Have a semester with no violations of academic integrity!");
+        System.out.println("My career ambitions is software engineer");
+        System.out.println("Be more active on LinkedIn");
+        System.out.println("Have a semester with no violations of academic integrity!");
         System.out.println();
-        System.out.println("My hobbies:");
-        System.out.println("-- Investing");
-        System.out.println("-- Cooking");
-        System.out.println("-- Reading/Watching TV");
-        System.out.println("-- Riding my motorcycle");
+        System.out.println("My hobbies: listening music");
+        System.out.println("Investing");
+        System.out.println("Cooking");
+        System.out.println("Watching TV");
+        System.out.println("Riding my motorcycle");
         System.out.println();
     }
 }
